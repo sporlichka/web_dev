c1ec7cc1f514bbebbeb65a4653cf70ce98365882
sporlichka <tvooybatya@gmail.com>
2025-03-13 12:07:40 +0500

    lab6 changes

--- lab6/lab6-master/src/app/app.routes.ts
+++ lab6/lab6-master/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { AlbumComponent } from './album/album.component';
 import { AlbumDetailComponent } from './album-detail/album-detail.component';
@@ -6,9 +7,10 @@ import { PhotosComponent } from './photos/photos.component';
 import { AppComponent } from './app.component';  
 
 export const routes: Routes = [
+    { path: 'home', component: HomeComponent},
     { path: 'about', component: AboutComponent },
     { path: 'albums', component: AlbumComponent },
     { path: 'albums/:id', component: AlbumDetailComponent },
-    { path: 'albums/:id/photos', component: PhotosComponent },
-    { path: '**', redirectTo: '' } 
+    { path: 'albums/:id/photos', component: PhotosComponent }, 
+    { path: '**', redirectTo: '/home' }
 ];
